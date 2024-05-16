2ec9b0e6d28d8d217abfb226a02934a996787d53
Bonevy BEBY <bonevybeby@gmail.com>
2024-05-16 20:31:44 +0200

    add TestAppFX//Thread

--- src/main/java/com/formationkilo/javafxproject/TestAppFX.java
+++ src/main/java/com/formationkilo/javafxproject/TestAppFX.java
@@ -1,15 +1,58 @@
 package com.formationkilo.javafxproject;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class TestAppFX extends Application {
     @Override
-    public void start(Stage window) throws Exception {
+    public void start(Stage winPrimary) throws Exception {
+        winPrimary.setTitle("First window");
+        winPrimary.setWidth(600);
+        winPrimary.setHeight(400);
+        winPrimary.setResizable(false);
+        winPrimary.show();
+        winPrimary.centerOnScreen();
+
+        Stage window=new Stage();
+
        window.setTitle("I love JavaFX");
        window.setWidth(400);
-        window.setHeight(300);
-        window.show();
-        window.centerOnScreen();
+       window.setHeight(300);
+       window.setResizable(false);
+       window.show();
+       window.centerOnScreen();
+
+       //To kwo the current
+        //Thread.sleep(5000);
+
+      // System.out.println(Thread.currentThread().getName()); //JavaFX Application Thread
+         System.out.println("Before the Thread...");
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               try {
+                   System.out.println("In the Thread...");
+                   Thread.sleep(5000);
+                  // window.close();
+                   Platform.runLater(new Runnable() {
+                       @Override
+                       public void run() {
+                          System.out.println(Thread.currentThread().getName());  //JavaFX Application Thread
+                          window.close(); 
+                       }
+                   });
+               } catch (InterruptedException e) {
+                   throw new RuntimeException(e);
+               }
+
+           }
+       }).start();
+        System.out.println("After the Thread...");
+
     }
 }
+
+
+
+
---
+++ src/main/java/com/formationkilo/javafxproject/TestAppFXLambdaExpression.java
@@ -0,0 +1,64 @@
+package com.formationkilo.javafxproject;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class TestAppFXLambdaExpression extends Application {
+    @Override
+    public void start(Stage winPrimary) throws Exception {
+        winPrimary.setTitle("First window");
+        winPrimary.setWidth(600);
+        winPrimary.setHeight(400);
+        winPrimary.setResizable(false);
+        winPrimary.show();
+        winPrimary.centerOnScreen();
+
+        Stage window=new Stage();
+
+       window.setTitle("I love JavaFX");
+       window.setWidth(400);
+       window.setHeight(300);
+       window.setResizable(false);
+       window.show();
+       window.centerOnScreen();
+
+       //To kwo the current
+        //Thread.sleep(5000);
+
+      // System.out.println(Thread.currentThread().getName()); //JavaFX Application Thread
+         System.out.println("Before the Thread...");
+       /*  new Thread(()->{
+             try {
+                 System.out.println("In the Thread...");
+                 Thread.sleep(5000);
+                 Platform.runLater(()->{
+                     System.out.println(Thread.currentThread().getName());  //JavaFX Application Thread
+                     window.close();
+                 });
+             } catch (InterruptedException e) {
+                 throw new RuntimeException(e);
+             }
+
+         }).start();*/
+
+        new Thread(()->{
+            try {
+                System.out.println("In the Thread...");
+                Thread.sleep(5000);
+                Platform.runLater(()->{
+                    System.out.println(Thread.currentThread().getName());  //JavaFX Application Thread
+                    window.close();
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }).start();
+
+    }
+}
+
+
+
+
